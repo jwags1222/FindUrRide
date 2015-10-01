b70e127672d3d11882ee53d1c3e6b3abf054c419
jwags1222 <jtwagner1@gmail.com>
2015-10-01 10:17:38 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Prospect.create(name: 'Karen')
+Prospect.create(name: 'Steph')
+Pospect.create(name: 'Jen')
