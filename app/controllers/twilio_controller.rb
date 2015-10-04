3982b7d676500619b0e7283275354425899b256b
jwags1222 <jtwagner1@gmail.com>
2015-10-04 06:49:43 -0400

    added help feature

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -39,15 +39,15 @@ class TwilioController < ApplicationController
         @twilio_client.account.messages.create(:from => "+1#{twilio_phone_number}", :to => from_number, :body => " \n\nHello from Used Car World!! \n\nThank you for your interest in the #{car_requested.last.year}, #{car_requested.last.make} . Please click on this link to see a price and other information on your car #{car_requested.last.link}. \n\nAlso, you can use this link https://www.linkedin.com/pub/jayson-hobbs/3/713/8b9 to check the value of your trade in.  Enjoy your new car!")
         @twilio_client.account.messages.create(:from => "+1#{twilio_phone_number}", :to => '4124273378', :body => " \n\nAnother lead from LeadFeed!  \n\n#{from_number} texted us about the #{car_requested.last.year},  #{car_requested.last.make} #{car_requested.last.model}. You will receive an email update with all of your leads at the end of the day.  \n\nThank you for your business")
     
-    #elsif message_body == "HELP" || "Help"
+    elsif message_body == "HELP" || message_body == "Help"
     
-        #@twilio_client.account.messages.create(:from => "+1#{twilio_phone_number}", :to => from_number, :body => " \n\nHello from Used Car World!! \n\nOoops! We're sorry but your text didn't match any of our cars.  #{message_body} was the text that we recieved.  Please check to make sure that there are no extra spaces in your text and that the numbers are correct.  Text HELP if you would like additional help.")
-        #@twilio_client.account.messages.create(:from => "+1#{twilio_phone_number}", :to => '4124273378', :body => " \n\nAnother lead from LeadFeed!  \n\n#{from_number} is on your lot and and texted us about a car, but what they sent us doesnt match our records.  Usually this is because somone f")
+        @twilio_client.account.messages.create(:from => "+1#{twilio_phone_number}", :to => from_number, :body => " \n\nHello from Used Car World!! \n\nSorry that you are having trouble. Someone will be reaching out to you shortly.")
+        @twilio_client.account.messages.create(:from => "+1#{twilio_phone_number}", :to => '4124273378', :body => " \n\nAnother lead from LeadFeed!  \n\nA prospect with the number #{from_number} is on your lot and texted us about a car. But they are having trouble and they would like for some additional help. Could be a hot lead if you call them now!")
     
     else
     
         @twilio_client.account.messages.create(:from => "+1#{twilio_phone_number}", :to => from_number, :body => " \n\nHello from Used Car World!! \n\nOoops! We're sorry but your text didn't match any of our cars.  #{message_body} was the text that we recieved.  Please check to make sure that there are no extra spaces in your text and that the numbers are correct.  Text HELP if you would like additional help.")
-        @twilio_client.account.messages.create(:from => "+1#{twilio_phone_number}", :to => '4124273378', :body => " \n\nAnother lead from LeadFeed!  \n\n#{from_number} is on your lot and and texted us about a car, but what they sent us doesnt match our records.  Usually this is because somone fat fingered a number or added a space.  Please reach out when you can.\n\nAs always, thank you for your business!")
+        @twilio_client.account.messages.create(:from => "+1#{twilio_phone_number}", :to => '4124273378', :body => " \n\nAnother lead from LeadFeed!  \n\nA prospect with the number #{from_number} is on your lot and and texted us about a car but what they sent us doesnt match our records.  Usually this is because somone fat fingered a number or added a space.  Please reach out when you can.\n\nAs always, thank you for your business!")
     end 
 
 
